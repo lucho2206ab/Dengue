26eec688c999dd827f53f4b18a8815b3fe61c53d
Luis <luis.abreumza@gmail.com>
2024-03-25 17:04:12 -0300

    Commit inicial

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(3000);
+console.log('Puerto 3000 esta corriendo');
